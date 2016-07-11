a5a733ed1b0316a0a14af479563d824c5d423574
ChenShi <280580216@qq.com>
2016-07-11 09:59:52 +0800

    learn to python
    菜鸟学python .
    
    1.输出格式化的字符串
    %d  整数
    %f  浮点数
    %s  字符串
    %x  十六进制整数

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+#print:
+# a = -111
+# if a >= 0:
+#     print(a)
+# else:
+#     print(-a)
+#
+# a = "abc"
+# b = a
+# a = "XYZ"
+# print(b)
+
+# print("包括中文的编码")
+# print()
+
+# print('\u4e2d\u6587')
+
+# print("hello, %s" % "world")
+r = 72 / 85 * 100
+print('小明成绩提升了%.1f %%' % (r))
