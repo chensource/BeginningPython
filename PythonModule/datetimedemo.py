e7da67d211b7b3c509aea291aa30167df437324a
Stone <280580216@qq.com>
2016-07-25 00:34:36 +0800

    PythonModule/ReadMe.md  -- About python 内置模块  	PythonModule/__pycache__/  	Regex/  -- 正则表达式

---
+++ PythonModule/datetimedemo.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+now = datetime.now()
+print(now.strftime('%Y-%m-%d %H:%M:%S %a,%b'))
+print(type(now))
+
+timestamp = now.timestamp()
+print(timestamp)
+
+cday = datetime.strptime('2016-07-25 00:29:57','%Y-%m-%d %H:%M:%S')
+print(cday)
