3956ac794f83f35eb3a2f4fba0365aa85d412f08
ChenShi <280580216@qq.com>
2016-08-02 13:38:35 +0800

    python 绘图 &模拟http 请求
    python 绘图 &模拟http 请求

---
+++ PythonModule/urllibdemo.py
@@ -0,0 +1,48 @@
+from urllib import request,parse
+
+URL_INDEX = 'http://www.douban.com'
+URL_LOGIN = 'https://passport.weibo.cn/sso/login'
+# with request.urlopen(URL_INDEX) as f:
+#     data = f.read()
+#     print('status:', f.status, f.reason)
+#     for key, value in f.getheaders():
+#         print('%s:%s' % (key, value))
+#     print('data:', data.decode('utf-8'))
+
+#Get 请求
+# req = request.Request(URL_INDEX)
+# req.add_header(
+#     'User-Agent',
+#     'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# with request.urlopen(URL_INDEX) as f:
+#     data = f.read()
+#     print('status:', f.status, f.reason)
+#     for key, value in f.getheaders():
+#         print('%s:%s' % (key, value))
+#     print('data:', data.decode('utf-8'))
+
+print('Login in Weibo...')
+email = input('Email:')
+password = input('Password:')
+login_data = parse.urlencode([
+    ('username', email), ('password', password), ('entry', 'mweibo'),
+    ('client_id', ''), ('savestate', '1'), ('ec', ''),
+    ('pagerefer',
+     'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F'
+     )
+])
+
+req = request.Request(URL_LOGIN)
+req.add_header('Origin', 'https://passwort.weibo.cn')
+req.add_header(
+    'User-Agent',
+    'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req.add_header(
+    'referer',
+    'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+
+with request.urlopen(req, data=login_data.encode('utf-8')) as f:
+    print("Status: ", f.status, f.reason)
+    for key, value in f.getheaders():
+        print('%s:%s' % (key, value))
+    print('Data:', f.read().decode('utf-8'))
--- module/PILImage/thumbail.py
+++ module/PILImage/thumbail.py
@@ -1,9 +1,54 @@
-from PIL import Image
+# from PIL import Image, ImageFilter
 
 '生成缩略图'
 
-im = Image.open("test.png")
-print(im.format, im.size, im.mode)
-# PNG(400,300) RGB
-im.thumbnail((200, 100))
-im.save("thumb.jpg", "JPEG")
+# im = Image.open("test.png")
+# print(im.format, im.size, im.mode)
+# # PNG(400,300) RGB
+# im.thumbnail((200, 100))
+# im.save("thumb.jpg", "JPEG")
+
+'模糊图片'
+# im = Image.open('test.png')
+# im2 = im.filter(ImageFilter.BLUR)
+# im2.save('blur.jpg', 'JPEG')
+
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+import random
+
+
+#随机字母
+def rndChar():
+    return chr(random.randint(65, 90))
+
+
+#随机颜色
+def rndColor():
+    return (random.randint(64, 255), random.randint(64, 255),
+            random.randint(64, 255))
+
+
+def rndColor2():
+    return (random.randint(32, 127), random.randint(32, 127),
+            random.randint(32, 127))
+
+
+width = 64 * 4
+height = 60
+
+image = Image.new('RGB', (width, height), (255, 255, 255))
+#创建Font对象
+font = ImageFont.truetype('C:\\Windows\\Fonts\\Arial.ttf', 36)
+draw = ImageDraw.Draw(image)
+#填充元素
+for x in range(width):
+    for y in range(height):
+        draw.point((x, y), fill=rndColor())
+
+#输出文字
+for t in range(4):
+    draw.text((60 * t + 10, 10), rndChar(), font=font, fill=rndColor2())
+
+#模糊
+image = image.filter(ImageFilter.BLUR)
+image.save('code.jpg', 'JPEG')
