d84da5d7c233df840aeb26f421db0d09616a419b
ChenShi <280580216@qq.com>
2016-08-15 18:03:30 +0800

    1.urls & template
    https://docs.djangoproject.com/en/1.10/intro/tutorial03/

--- chensource-django-webapp/mysite/polls/urls.py
+++ chensource-django-webapp/mysite/polls/urls.py
@@ -1,4 +1,17 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    # eg: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)',
+        views.detail, name='detail'),
+    # eg : /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$',
+        views.results,
+        name='results'),
+    # eg : /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$',
+        views.vote, name='vote')
+]
