5fa1ba0f37c121d71357e1a70ae2950bc0d68c2e
ChenShi <280580216@qq.com>
2016-08-26 18:18:19 +0800

    update python day 2
    完成论坛demo .
    
    vue.
    html 5
    flask

--- awesome-python3-webapp/www/app.py
+++ awesome-python3-webapp/www/app.py
@@ -1,24 +1,21 @@
 import logging
-import asyncio
-import json
-import os
-import time
-from aiohttp import web
+logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
 from datetime import datetime
+
+from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 
 import orm
 from coroweb import add_routes, add_static
+from handlers import COOKIE_NAME, cookie2user
 
 logging.basicConfig(level=logging.INFO)
 
-# def index(request):
-#     return web.Response(body='你好'.encode(encoding='utf-8'),
-#                         headers={'Content-Type': 'text/html;charset=utf-8'})
-
 
 def init_jinja2(app, **kw):
-    logging.info('init jinja2....')
+    logging.info('init jinja2...')
     options = dict(autoescape=kw.get('autoescape', True),
                    block_start_string=kw.get('block_start_string', '{%'),
                    block_end_string=kw.get('block_end_string', '%}'),
@@ -26,7 +23,6 @@ def init_jinja2(app, **kw):
                    variable_end_string=kw.get('variable_end_string', '}}'),
                    auto_reload=kw.get('auto_reload', True))
     path = kw.get('path', None)
-    # logging.info('set jinja2 template path: %s' % path)
     if path is None:
         path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -35,42 +31,62 @@ def init_jinja2(app, **kw):
     filters = kw.get('filters', None)
     if filters is not None:
         for name, f in filters.items():
+            logging.info('set jinja2 filters name: %s, filters:%s' % (name, f))
             env.filters[name] = f
-        app['__templates__'] = env
+
+    app['__templating__'] = env
 
 
-@asyncio.coroutine
-def logger_factory(app, handler):
-    def logger(request):
+async def logger_factory(app, handler):
+
+    async def logger(request):
         logging.info('Request: %s %s' % (request.method, request.path))
         # await asyncio.sleep(0.3)
-        return handler(request)
+        return (await handler(request))
 
     return logger
 
 
-@asyncio.coroutine
-def data_factory(app, handler):
-    def parse_data(request):
+async def auth_factory(app, handler):
+
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user:%s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (
+                request.__uesr__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+
+    return auth
+
+
+async def data_factory(app, handler):
+
+    async def parse_data(request):
         if request.method == 'POST':
             if request.content_type.startswith('application/json'):
-                request.__data__ = request.json()
+                request.__data__ = await request.json()
                 logging.info('request json: %s' % str(request.__data__))
             elif request.content_type.startswith(
                     'application/x-www-form-urlencoded'):
-                request.__data__ = request.post()
+                request.__data__ = await request.post()
                 logging.info('request form: %s' % str(request.__data__))
-        return handler(request)
+        return (await handler(request))
 
     return parse_data
 
 
-@asyncio.coroutine
-def response_factory(app, handler):
-    def response(request):
+async def response_factory(app, handler):
+
+    async def response(request):
         logging.info('Response handler...')
-        r = handler(request)
-        logging.info(type(r))
+        r = await handler(request)
         if isinstance(r, web.StreamResponse):
             return r
         if isinstance(r, bytes):
@@ -112,37 +128,36 @@ def response_factory(app, handler):
 
 
 def datetime_filter(t):
+    logging.info(t)
     delta = int(time.time() - t)
     if delta < 60:
-        return u'1分钟前'
+        return '1分钟前'
     if delta < 3600:
-        return u'%s分钟前' % (delta // 60)
+        return '%s分钟前' % (delta // 60)
     if delta < 86400:
-        return u'%s小时前' % (delta // 3600)
+        return '%s小时前' % (delta // 3600)
     if delta < 604800:
-        return u'%s天前' % (delta // 86400)
+        return '%s天前' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
-    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+    return '%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 
-@asyncio.coroutine
-def init(loop):
-    orm.create_pool(loop=loop,
-                    host='localhost',
-                    post=3306,
-                    user='sa',
-                    password='P@ssw0rd',
-                    db='awesome')
+async def init(loop):
+    await orm.create_pool(loop=loop,
+                          host='127.0.0.1',
+                          port=3306,
+                          user='sa',
+                          password='P@ssw0rd',
+                          db='awesome')
     app = web.Application(loop=loop,
                           middlewares=[
-                              logger_factory, response_factory
+                              logger_factory, auth_factory, response_factory
                           ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
     add_static(app)
-    # app.router.add_route("GET", "/", index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9003)
-    logging.info('Server started at http://127.0.0.1:9003')
+    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 9000)
+    logging.info('server started at http://0.0.0.0:9000...')
     return srv
 
 
